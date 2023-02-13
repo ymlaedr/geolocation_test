6d6b51fcbc99156adec2f49c1a6c3a8d45f703c5
ymlaedr <ymlaedr@gmail.com>
2023-02-13 12:13:01 +0900

    aarch64向けにdockerのpre_execを追加

--- init.sh
+++ init.sh
@@ -9,11 +9,37 @@
 PRJ_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd);
 
 PYTHON_EXEC_IMAGE='3.11.1-alpine3.17';
-PRE_EXEC_SCRIPT="pip install --upgrade pip && pip install -r requirements.txt";
+PRE_EXEC_SCRIPT_AARCH64="\
+	apk add \
+	  --no-cache \
+	  --update-cache \
+	  --virtual .exec_pip_dependencies \
+	    alpine-sdk \
+	    musl-dev \
+	    libffi-dev \
+	 && \
+	apk add \
+	  --no-cache \
+	  --update-cache \
+	  --virtual .install_pip_requirements_dependencies \
+	    alpine-sdk";
+PRE_EXEC_SCRIPT="\
+	pip install --upgrade pip \
+	 && \
+	pip install -r requirements.txt";
+POST_EXEC_SCRIPT_AARCH64="\
+	apk del --purge .install_pip_requirements_dependencies";
+[ "$(arch)" = 'aarch64' ] && PRE_EXEC_SCRIPT="\
+	${PRE_EXEC_SCRIPT_AARCH64} \
+	 && \
+	${PRE_EXEC_SCRIPT} \
+	 && \
+	${POST_EXEC_SCRIPT_AARCH64}";
 EXEC_SCRIPT_APP="${PRE_EXEC_SCRIPT} && python app.py";
 EXEC_SCRIPT_SH="${PRE_EXEC_SCRIPT} && sh";
-EXEC_SCRIPT_DB_UPGRADE="${PRE_EXEC_SCRIPT} && flask db upgrade"
+EXEC_SCRIPT_DB_UPGRADE="${PRE_EXEC_SCRIPT} && flask db upgrade";
 SQLITE3_PATH='${PRJ_DIR}/geolocation_test.sqlite3';
+
 
 
 ## --------------------------------------------------------------------------
